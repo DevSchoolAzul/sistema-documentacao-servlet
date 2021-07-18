efc3b8b2aa6191ec30c88031398cbdfff98963e1
Brunno <brunnosantana98@gmail.com>
2021-07-18 00:42:43 -0300

    fix nas classes de Tela e TelaDao

--- src/main/java/br/com/sistemaDocumentacao/dao/TelaDao.java
+++ src/main/java/br/com/sistemaDocumentacao/dao/TelaDao.java
@@ -5,29 +5,34 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 import br.com.sistemaDocumentacao.modelo.Tela;
 
 public class TelaDao {
-	
-	
+
 	private Connection connection;
-		
+
 	public TelaDao(Connection connection) {
 		this.connection = connection;
 	}
-		
+
 	public void cadastrar(Tela tela) {
 		String sql = "INSERT INTO tela (Id_versao,Data_Cadastro,Nome_Tela,Imagem,Situacao,Tela_Pai,Ordem,Url_log,Versao_Origem)"
 				+ " " + "values (?,?,?,?,?,?,?,?,?)";
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 			pstm.setInt(1, tela.getId_versao());
-			pstm.setDate(2, (Date) tela.getData_Cadastro());
+			pstm.setDate(2, Date.valueOf(tela.getData_Cadastro()));
 			pstm.setString(3, tela.getNome_Tela());
-			pstm.setString(4,tela.getImagem());
-			pstm.setBoolean(5,tela.isSituacao());
-			pstm.setInt(6, tela.getTela_Pai());
+			pstm.setString(4, tela.getImagem());
+			pstm.setBoolean(5, tela.isSituacao());
+			if (tela.getTela_Pai() == 0) {
+				pstm.setNull(6, 0);
+			} else {
+				pstm.setInt(6, tela.getTela_Pai());
+			}
 			pstm.setInt(7, tela.getOrdem());
 			pstm.setString(8, tela.getUrl_log());
 			pstm.setInt(9, tela.getVersao_Origem());
@@ -36,18 +41,22 @@ public class TelaDao {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public void atualizar(Tela tela) {
 		String sql = "UPDATE tela SET  Id_versao = ?, Data_Cadastro = ?, Nome_Tela = ?, Imagem = ?,"
 				+ " Situacao = ?, Tela_Pai = ?, Ordem = ?, Url_log = ? ,Versao_Origem = ? WHERE id_tela = ?";
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 			pstm.setInt(10, tela.getId_tela());
 			pstm.setInt(1, tela.getId_versao());
-			pstm.setDate(2, (Date) tela.getData_Cadastro());
+			pstm.setDate(2, Date.valueOf(tela.getData_Cadastro()));
 			pstm.setString(3, tela.getNome_Tela());
-			pstm.setString(4,tela.getImagem());
-			pstm.setBoolean(5,tela.isSituacao());
-			pstm.setInt(6, tela.getTela_Pai());
+			pstm.setString(4, tela.getImagem());
+			pstm.setBoolean(5, tela.isSituacao());
+			if (tela.getTela_Pai() == 0) {
+				pstm.setNull(6, 0);
+			} else {
+				pstm.setInt(6, tela.getTela_Pai());
+			}
 			pstm.setInt(7, tela.getOrdem());
 			pstm.setString(8, tela.getUrl_log());
 			pstm.setInt(9, tela.getVersao_Origem());
@@ -56,42 +65,39 @@ public class TelaDao {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public void excluir(int id_tela) {
 		String sql = "DELETE FROM tela WHERE id_tela = ?";
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
-			pstm.setInt(1,id_tela);
+			pstm.setInt(1, id_tela);
 			pstm.execute();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
 
-	public  Tela buscarPorId(int id) {
-		String sql = "SELECT Id_versao,Data_Cadastro,Nome_Tela,Imagem,Situacao,Tela_Pai,Ordem,Url_log,Versao_Origem"
-				+ " FROM projeto WHERE id_tela = ?";
+	public Tela buscarPorId(int id) {
+		String sql = "SELECT Id_versao, Data_Cadastro, Nome_Tela, Imagem, Situacao, "
+				+ " Tela_Pai, Ordem, Url_log, Versao_Origem FROM tela WHERE id_tela = ? ";
 		Tela tela = null;
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 			pstm.setInt(1, id);
 			pstm.execute();
 			try (ResultSet result = pstm.getResultSet()) {
 				while (result.next()) {
-					String Id_Tela = result.getString(1);
-					String Id_versao = result.getString(2);
-					Date Data_Cadastro = result.getDate(3);
-					String Nome_Tela = result.getString(4);
-					String Imagem = result.getString(5);
-					Boolean Situacao = result.getBoolean(6);
-					Integer Tela_Pai = result.getInt(7);
-					Integer Ordem = result.getInt(8);
-					String Url_log = result.getString(9);
-					Integer Versao_Origem= result.getInt(10);		
+					Integer Id_versao = result.getInt(1);
+					Date Data_Cadastro = result.getDate(2);
+					String Nome_Tela = result.getString(3);
+					String Imagem = result.getString(4);
+					Boolean Situacao = result.getBoolean(5);
+					Integer Tela_Pai = result.getInt(6);
+					Integer Ordem = result.getInt(7);
+					String Url_log = result.getString(8);
+					Integer Versao_Origem = result.getInt(9);
 					tela = new Tela();
 					tela.setId_tela(id);
-					tela.setId_versao(id);
-					tela.setData_Cadastro(Data_Cadastro);
+					tela.setId_versao(Id_versao);
+					tela.setData_Cadastro(Data_Cadastro.toLocalDate());
 					tela.setNome_Tela(Nome_Tela);
 					tela.setImagem(Imagem);
 					tela.setSituacao(Situacao);
@@ -101,15 +107,13 @@ public class TelaDao {
 					tela.setVersao_Origem(Versao_Origem);
 				}
 			}
-			
-			}
-				catch (SQLException e) {
-					throw new RuntimeException(e);
-		   }
-				System.out.println(tela);
-					return tela;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return tela;
 	}
-		
+
 	public List<Tela> listarTela() {
 		String sql = "SELECT * FROM tela";
 		List<Tela> telas = new ArrayList<>();
@@ -126,12 +130,12 @@ public class TelaDao {
 					Integer Tela_Pai = result.getInt(7);
 					Integer Ordem = result.getInt(8);
 					String Url_log = result.getString(9);
-					Integer Versao_Origem= result.getInt(10);
+					Integer Versao_Origem = result.getInt(10);
 					Tela tela = new Tela();
-					
+
 					tela.setId_tela(1);
 					tela.setId_versao(2);
-					tela.setData_Cadastro(Data_Cadastro);
+					tela.setData_Cadastro(Data_Cadastro.toLocalDate());
 					tela.setNome_Tela(Nome_Tela);
 					tela.setImagem(Imagem);
 					tela.setSituacao(Situacao);
@@ -139,7 +143,7 @@ public class TelaDao {
 					tela.setOrdem(Ordem);
 					tela.setUrl_log(Url_log);
 					tela.setVersao_Origem(Versao_Origem);
-			}
+				}
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -147,10 +151,3 @@ public class TelaDao {
 		return telas;
 	}
 }
-
-	
-	
-	
-	
-	
-	
--- src/main/java/br/com/sistemaDocumentacao/modelo/Tela.java
+++ src/main/java/br/com/sistemaDocumentacao/modelo/Tela.java
@@ -1,86 +1,101 @@
 package br.com.sistemaDocumentacao.modelo;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 //(Id_tela,Id_versao,Data_Cadastro,Nome_Tela,Imagem,Situacao,Tela_Pai,Ordem,Url_log,Versao_Origem)
 public class Tela {
-	
-	private int  Id_tela;
-	private int  Id_versao;
-	private Date  Data_Cadastro;
+
+	private int Id_tela;
+	private int Id_versao;
+	private LocalDate Data_Cadastro;
 	private String Nome_Tela;
-	private  String Imagem;
+	private String Imagem;
 	private boolean Situacao;
 	private int Tela_Pai;
 	private int Ordem;
 	private String Url_log;
 	private int Versao_Origem;
-	
-	
-	//metodos get e set
-	
-	
+
+	// metodos get e set
+
 	public int getId_tela() {
 		return Id_tela;
 	}
+
 	public void setId_tela(int id_tela) {
 		Id_tela = id_tela;
 	}
+
 	public int getId_versao() {
 		return Id_versao;
 	}
+
 	public void setId_versao(int id_versao) {
 		Id_versao = id_versao;
 	}
-	public Date getData_Cadastro() {
-		return Data_Cadastro;
-	}
-	public void setData_Cadastro(Date data_Cadastro) {
-		Data_Cadastro = data_Cadastro;
-	}
+
 	public String getNome_Tela() {
 		return Nome_Tela;
 	}
+
 	public void setNome_Tela(String nome_Tela) {
 		Nome_Tela = nome_Tela;
 	}
+
 	public String getImagem() {
 		return Imagem;
 	}
+
 	public void setImagem(String imagem) {
 		Imagem = imagem;
 	}
+
 	public boolean isSituacao() {
 		return Situacao;
 	}
+
 	public void setSituacao(boolean situacao) {
 		Situacao = situacao;
 	}
+
 	public int getTela_Pai() {
 		return Tela_Pai;
 	}
+
 	public void setTela_Pai(int tela_Pai) {
 		Tela_Pai = tela_Pai;
 	}
+
 	public int getOrdem() {
 		return Ordem;
 	}
+
 	public void setOrdem(int ordem) {
 		Ordem = ordem;
 	}
+
 	public String getUrl_log() {
 		return Url_log;
 	}
+
 	public void setUrl_log(String url_log) {
 		Url_log = url_log;
 	}
+
 	public int getVersao_Origem() {
 		return Versao_Origem;
 	}
+
 	public void setVersao_Origem(int versao_Origem) {
 		Versao_Origem = versao_Origem;
 	}
-	
-	
-	
-	
+
+	public void setData_Cadastro(LocalDate data_Cadastro) {
+		Data_Cadastro = data_Cadastro;
+	}
+
+	public LocalDate getData_Cadastro() {
+		return Data_Cadastro;
+	}
+
 }
