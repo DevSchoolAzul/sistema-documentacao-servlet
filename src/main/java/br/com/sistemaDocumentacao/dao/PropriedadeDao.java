84bdd94de95054cdaeb2f8cd34d8ccb6ab3a5852
Brunno <brunnosantana98@gmail.com>
2021-07-23 00:07:41 -0300

    modelo e dao de Propriedade de Requisicao

--- src/main/java/br/com/sistemaDocumentacao/dao/PropriedadeDao.java
+++ src/main/java/br/com/sistemaDocumentacao/dao/PropriedadeDao.java
@@ -1,5 +1,117 @@
 package br.com.sistemaDocumentacao.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.sistemaDocumentacao.modelo.Propriedade;
+
 public class PropriedadeDao {
 
+	private Connection connection;
+	
+	public PropriedadeDao(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public void cadastrar(Propriedade propriedade) {
+		String sql = "INSERT INTO propriedade"
+				+ "(id_requisicao, chave, valor, ordem) "
+				+ "values (?, ?, ?, ?)";
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.setInt(1, propriedade.getIdRequisicao());
+			pstm.setString(2, propriedade.getChave());
+			pstm.setString(3, propriedade.getValor());
+			pstm.setInt(4, propriedade.getOrdem());
+			pstm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void atualizar(Propriedade propriedade) {
+		/* não coloquei pra alterar id_requisição pois não perece
+		 correto alterar esse atributo */
+		String sql = "UPDATE propriedade SET chave = ?, valor = ?, ordem = ?"
+				+ " WHERE id_propriedade = ?";
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.setString(1, propriedade.getChave());
+			pstm.setString(2, propriedade.getValor());
+			pstm.setInt(3, propriedade.getOrdem());
+			pstm.setInt(4, propriedade.getIdPropriedade());
+			pstm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void excluir(int idPropriedade) {
+		String sql = "DELETE FROM propriedade WHERE id_propriedade = ?";
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.setInt(1, idPropriedade);
+			pstm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Propriedade buscarPorId(int id) {
+		String sql = "SELECT id_requisicao, chave, valor, ordem FROM propriedade"
+				+ " WHERE id_propriedade = ?";
+		Propriedade propriedade = null;
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.setInt(1, id);
+			pstm.execute();
+			try (ResultSet result = pstm.getResultSet()) {
+				while (result.next()) {
+					Integer idRequisicao = result.getInt(1);
+					String chave = result.getString(2);
+					String valor = result.getString(3);
+					Integer ordem = result.getInt(4);
+					propriedade = new Propriedade();
+					propriedade.setIdPropriedade(id);
+					propriedade.setIdRequisicao(idRequisicao);
+					propriedade.setChave(chave);
+					propriedade.setValor(valor);
+					propriedade.setOrdem(ordem);
+				}
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return propriedade;
+	}
+	
+	public List<Propriedade> ListarPropriedades() {
+		String sql = "SELECT id_propriedade, id_requisicao, chave, valor, ordem "
+				+ "FROM propriedade";
+		List<Propriedade> propriedades = null;
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+			try(ResultSet result = pstm.getResultSet()) {
+				while(result.next()) {
+					Integer idPropriedade = result.getInt(1);
+					Integer idRequisicao = result.getInt(2);
+					String chave = result.getString(3);
+					String valor = result.getString(4);
+					Integer ordem = result.getInt(5);
+					Propriedade propriedade = new Propriedade();
+					propriedade.setIdPropriedade(idPropriedade);
+					propriedade.setIdRequisicao(idRequisicao);
+					propriedade.setChave(chave);
+					propriedade.setValor(valor);
+					propriedade.setOrdem(ordem);
+					
+					propriedades.add(propriedade);
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return propriedades;
+	}
 }
--- src/main/java/br/com/sistemaDocumentacao/modelo/Propriedade.java
+++ src/main/java/br/com/sistemaDocumentacao/modelo/Propriedade.java
@@ -2,4 +2,50 @@ package br.com.sistemaDocumentacao.modelo;
 
 public class Propriedade {
 
+	private Integer idPropriedade;
+	private Integer idRequisicao;
+	private String chave;
+	private String valor;
+	private Integer ordem;
+
+	public Integer getIdPropriedade() {
+		return idPropriedade;
+	}
+
+	public void setIdPropriedade(Integer idPropriedade) {
+		this.idPropriedade = idPropriedade;
+	}
+
+	public Integer getIdRequisicao() {
+		return idRequisicao;
+	}
+
+	public void setIdRequisicao(Integer idRequisicao) {
+		this.idRequisicao = idRequisicao;
+	}
+
+	public String getChave() {
+		return chave;
+	}
+
+	public void setChave(String chave) {
+		this.chave = chave;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public Integer getOrdem() {
+		return ordem;
+	}
+
+	public void setOrdem(Integer ordem) {
+		this.ordem = ordem;
+	}
+
 }
